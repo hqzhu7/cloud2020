ed6ab65e8e491049e508b8d4a7f0323fe0a5b3d4
hq zhu <hq.zhu7@gmail.com>
2020-06-20 22:33:34 -0400

    add eureka server/client

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/config/ApplicationContextConfig.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/config/ApplicationContextConfig.java
@@ -1,10 +1,12 @@
 package com.atguigu.springcloud.config;
 
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
+@EnableEurekaClient
 public class ApplicationContextConfig
 {
     @Bean
